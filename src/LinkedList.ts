a5e57e56c05c89eb08fa02f1680ef50b3bdbafb3
wackyjoker <kevin.young8931@gmail.com>
2021-08-19 05:17:30 +0200

    adding append function

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -3,15 +3,32 @@ export class Node<T> {
 	public constructor(public value: T) {}
 }
 
-interface ILinkedList<T> {}
+interface ILinkedList<T> {
+	append(data: T): Node<T>
+}
 
 class LinkedList<T> implements ILinkedList<T> {
 	private head: Node<T> | null = null
-	constructor(value: T) {
-		this.head = new Node(value)
+	private tail: Node<T> | null = null
+	private length: number = 0
+
+	public append(data: T) {
+		const newNode = new Node(data)
+		if (!this.head) {
+			this.head = newNode
+			this.tail = this.head
+			return newNode
+		}
+
+		this.tail.next = newNode
+		this.tail = newNode
+		this.length++
+		return newNode
 	}
 }
 
-const linkedList = new LinkedList(2)
+const linkedList = new LinkedList()
 
+linkedList.append(1)
+linkedList.append(2)
 console.log(linkedList)
