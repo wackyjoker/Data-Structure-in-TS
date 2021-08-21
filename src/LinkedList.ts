e4bf13243c91d5970652693fa34f32372f2014d1
wackyjoker <kevin.young8931@gmail.com>
2021-08-21 20:52:18 +0200

    added insert and traverse function

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -6,6 +6,9 @@ export class Node<T> {
 interface ILinkedList<T> {
 	append(data: T): this
 	prepend(data: T): this
+	printList(): Array<T>
+	traverse(index: number): Node<T>
+	insert(index: number, data: T): this
 }
 
 class LinkedList<T> implements ILinkedList<T> {
@@ -41,6 +44,33 @@ class LinkedList<T> implements ILinkedList<T> {
 		this.length++
 		return this
 	}
+	public printList() {
+		const array = []
+		let currentNode = this.head
+		while (currentNode !== null) {
+			array.push(currentNode.value)
+			currentNode = currentNode.next
+		}
+		return array
+	}
+	public traverse(index: number) {
+		let currentNode = this.head
+		if (!currentNode) return null
+		for (let i = 0; i < index; i++) {
+			currentNode = currentNode.next
+		}
+		return currentNode
+	}
+
+	public insert(index: number, data: T) {
+		const newNode = new Node(data)
+		const head = this.traverse(index)
+		const temp = head.next
+		head.next = newNode
+		newNode.next = temp
+		this.length++
+		return this
+	}
 }
 
 const linkedList = new LinkedList()
@@ -50,3 +80,8 @@ linkedList.append(3)
 linkedList.prepend(1)
 linkedList.append(5)
 console.log(linkedList)
+console.log(linkedList.printList())
+console.log(linkedList.traverse(2))
+linkedList.insert(2, 4)
+console.log(linkedList)
+console.log(linkedList.printList())
